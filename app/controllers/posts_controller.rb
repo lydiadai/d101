2ae19ea05668dc0f7f64fec749d0f5345bc4db11
lydiadai <lyida0511@gmail.com>
2016-10-27 20:24:57 +0800

    implement edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,34 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已刪除"
+  end
+
   private
+  def find_group
+      @group = Group.find(params[:group_id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
