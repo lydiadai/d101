45a62ab96b7428df8949a6e8530546f072a4a6a2
lydiadai <lyida0511@gmail.com>
2016-10-27 22:26:01 +0800

    implement my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   before_action :authenticate_user! ,only: [:new,:create]
+  before_action :find_group
   def new
-    @group = Group.find(params[:group_id])
+
     @post = Post.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
+    
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
@@ -17,8 +18,32 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
+    end
+ end
+
+ def destroy
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已刪除"
+end
 
   private
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
